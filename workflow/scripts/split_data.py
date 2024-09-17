88a83359d342bd2b66094b4f90845d4f63b376b7
Fangwen Zhao <fzhao@cemm.at>
2024-09-17 13:50:59 +0200

    Filter regions not by FilterByExpr but based on support

--- workflow/scripts/split_data.py
+++ workflow/scripts/split_data.py
@@ -10,6 +10,7 @@ import pandas as pd
 
 # input
 data_path = snakemake.input["data"]
+support_path = snakemake.input["support_path"]
 annotation_path = snakemake.input["annotation"]
 
 # parameters
@@ -19,12 +20,14 @@ split_by = snakemake.params["split_by"]
 
 # load data
 data = pd.read_csv(data_path, index_col=0)
+support = pd.read_csv(support_path, index_col=0)
 annotation = pd.read_csv(annotation_path, index_col=0)
 
 # split and save count and annotation data
 for split_var in split_by:
     if split_var == "all":
         data.loc[:, annotation.index].to_csv(os.path.join(result_path,"all","counts.csv"))
+        data.loc[:, annotation.index].to_csv(os.path.join(result_path,"all","support.csv"))
         annotation.to_csv(os.path.join(result_path,"all","annotation.csv"))
     
     if split_var in annotation.columns:
@@ -32,6 +35,9 @@ for split_var in split_by:
             
             # split & save counts
             data.loc[:, annotation[(annotation[split_var]==split)].index].to_csv(os.path.join(result_path,f"{split_var}_{split}","counts.csv"))
+
+            # split & save counts
+            support.loc[:, annotation[(annotation[split_var]==split)].index].to_csv(os.path.join(result_path,f"{split_var}_{split}","support.csv"))
     
             # split & save annotations
             annotation.loc[annotation[split_var]==split, :].to_csv(os.path.join(result_path,f"{split_var}_{split}","annotation.csv"))
